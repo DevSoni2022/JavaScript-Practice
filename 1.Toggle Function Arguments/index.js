0f31fdb32b7cc56e4c96d2c76241ad3f1fee827e
DevSoni2022 <121338813+DevSoni2022@users.noreply.github.com>
2023-04-01 23:34:27 +0530

    toggle on/off

---
+++ 1.Toggle Function Arguments/index.js
@@ -0,0 +1,20 @@
+const toggle  = (...args)=>{
+    let index = 0; 
+
+    return function(){
+        if(args.length){
+            if(index >= args.length)
+            {
+                index = 0;
+            }
+            return args[index++];
+            
+        }
+    }
+}
+const onOff = toggle("1","2","3")
+console.log(onOff())
+console.log(onOff())
+console.log(onOff())
+console.log(onOff())
+console.log(onOff())
